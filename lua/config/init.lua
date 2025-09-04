5fa9283de7564b49654b89d34c09a0a1de2f681c
= <luca.e@outlook.com>
2025-09-04 21:02:49 +0200

    move stuff around

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,10 +1,4 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-
 vim.g.mapleader = " "
 
-require("config.lazy")
+require('config.lazy')
+require('config.options')
