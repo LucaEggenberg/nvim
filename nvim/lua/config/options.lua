6ed46a1898d2c4e31d42a4f691cc50486ed181f9
= <luca.e@outlook.com>
2026-02-11 19:11:57 +0100

    enable breaks on textfiles

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,3 +5,12 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
+vim.opt.signcolumn = "yes:2"
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "text", },
+  callback = function ()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+  end,
+})
